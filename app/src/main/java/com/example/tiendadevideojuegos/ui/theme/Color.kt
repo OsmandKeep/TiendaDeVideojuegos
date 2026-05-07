e47089835ce3f30a04d1f0bf00fca8533aa1baed
Osmand <Osmandgtz3@gmail.com>
2026-05-07 15:28:32 -0600

    Creacion de paleta de colores para el tema blanco y negro

--- app/src/main/java/com/example/tiendadevideojuegos/ui/theme/Color.kt
+++ app/src/main/java/com/example/tiendadevideojuegos/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.tiendadevideojuegos.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFF6E8CFB)
+val PurpleGrey80 = Color(0xFF636CCB)
+val Pink80 = Color(0xFF50589C)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(0xFF635985)
+val PurpleGrey40 = Color(0xFF443C68)
+val Pink40 = Color(0xFF393053)
